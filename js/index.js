ab4ffbd776247c07ed601187bc594ead68d08792
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-08-24 18:41:21 +0200

    Added food & drinks section, added navigation color changing

--- js/index.js
+++ js/index.js
@@ -1,15 +1,62 @@
 window.onscroll = function() {
     scrollFunction()
+    changeNavigationColor()
 };
 
-// console.log(document.querySelector(".why-container").compareDocumentPosition())
+window.onload = function () {
+    like();
+}
 
 function scrollFunction() {
+    const navBrand = document.querySelector("nav .navbar-brand");
+    const logo = document.querySelector("nav .logo");
+
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.querySelector(".navbar-brand").style.fontSize = ".8rem";
-        document.querySelector(".logo").style.width = "0";
+        navBrand.style.fontSize = ".8rem";
+        logo.style.width = "0";
     } else {
-        document.querySelector(".navbar-brand").style.fontSize = "1.5rem";
-        document.querySelector(".logo").style.width = "2rem";
+        navBrand.style.fontSize = "1.5rem";
+        logo.style.width = "2rem";
     }
 }
+
+function changeNavigationColor() {
+    const navbar = document.querySelector("nav .navbar");
+    const navBrand = document.querySelector("nav .navbar-brand");
+    const navLinks = document.querySelectorAll("nav .nav-link");
+    const foodSection = document.getElementById('food-drinks');
+
+    const difference = foodSection.offsetHeight+ foodSection.offsetTop;
+
+    if (window.pageYOffset >= foodSection.offsetTop && window.pageYOffset < difference) {
+        navbar.classList.remove('bg-dark');
+        navbar.classList.add('bg-light');
+        navBrand.classList.remove('text-white');
+        navBrand.classList.add('text-dark');
+        navBrand.querySelector('span').classList.remove('bg-light');
+        navBrand.querySelector('span').classList.remove('text-dark');
+        navBrand.querySelector('span').classList.add('bg-dark');
+        navBrand.querySelector('span').classList.add('text-light');
+        navLinks.forEach(element => element.classList.remove('text-white'));
+        navLinks.forEach(element => element.classList.add('text-dark'));
+    } else {
+        navbar.classList.remove('bg-light');
+        navbar.classList.add('bg-dark');
+        navLinks.forEach(element => element.classList.remove('text-dark'));
+        navLinks.forEach(element => element.classList.add('text-white'));
+        navBrand.classList.remove('text-dark');
+        navBrand.classList.add('text-white');
+        navBrand.querySelector("span").classList.remove('bg-dark');
+        navBrand.querySelector("span").classList.remove('text-white');
+        navBrand.querySelector("span").classList.add('bg-light');
+        navBrand.querySelector("span").classList.add('text-dark');
+    }
+}
+
+function like() {
+    document.querySelectorAll(".heart").forEach(heart => heart.addEventListener("click", function() {
+            this.classList.toggle("is-active");
+        })
+    )
+}
+
